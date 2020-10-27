64e6f0f809a9de686ce1b15b46f4153d4916a877
cococolabear <794055465@qq.com>
2020-10-27 22:39:12 +0800

    StringBuilder

---
+++ src/main/java/com/example/javalanguaguetest/packageclass/PackageClassTest.java
@@ -0,0 +1,14 @@
+package com.example.javalanguaguetest.packageclass;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2020/10/27 12:25 上午
+ */
+public class PackageClassTest {
+    public static void main(String[] args) {
+        System.out.println(Integer.max(5,10));
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
