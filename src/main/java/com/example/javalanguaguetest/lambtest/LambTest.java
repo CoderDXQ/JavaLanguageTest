bb79f7bdbb48106c5a35b6d182ec3c600d12b262
cococolabear <794055465@qq.com>
2020-11-03 21:58:36 +0800

    lambda

---
+++ src/main/java/com/example/javalanguaguetest/lambtest/LambTest.java
@@ -0,0 +1,10 @@
+package com.example.javalanguaguetest.lambtest;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2020/11/3 9:49 下午
+ */
+//lambda表达式主要用于简化一些匿名内部类的写法 超过三行的逻辑不建议使用lambda表达式 语法糖
+public class LambTest {
+}
