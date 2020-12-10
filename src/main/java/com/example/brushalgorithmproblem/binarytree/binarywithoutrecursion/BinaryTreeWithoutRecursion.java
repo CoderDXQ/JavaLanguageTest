2056cea628803c295a744bc0c2c8dd6a0fb12315
cococolabear <794055465@qq.com>
2020-12-10 23:23:48 +0800

    使用stack的二叉树的非递归实现

--- src/main/java/com/example/brushalgorithmproblem/binarytree/binarywithoutrecursion/BinaryTreeWithoutRecursion.java
+++ src/main/java/com/example/brushalgorithmproblem/binarytree/binarywithoutrecursion/BinaryTreeWithoutRecursion.java
@@ -10,14 +10,31 @@ import java.util.Stack;
 public class BinaryTreeWithoutRecursion {
 
     //    内部类
-    public class Node {
+    public static class Node {
         public int value;
         public Node left;
         public Node right;
+
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
     }
 
     public static void main(String[] args) {
 
+        BinaryTreeWithoutRecursion bt = new BinaryTreeWithoutRecursion();
+
+        Node node4 = new Node(5, null, null);
+        Node node5 = new Node(2, null, null);
+        Node node2 = new Node(7, node4, null);
+        Node node3 = new Node(4, null, node5);
+        Node root1 = new Node(1, node2, node3);
+
+        bt.preOrder(root1);//输出结果：17542
+        bt.inOrder(root1);
+        bt.postOrder(root1);
     }
 
     public void preOrder(Node node) {
@@ -31,7 +48,7 @@ public class BinaryTreeWithoutRecursion {
 
         while (!stack.empty()) {
             Node curNode = stack.pop();
-            System.out.println(curNode.value);
+            System.out.print(curNode.value + " ");
             if (curNode.right != null) stack.push(curNode.right);
             if (curNode.left != null) stack.push(curNode.left);
         }
@@ -39,24 +56,27 @@ public class BinaryTreeWithoutRecursion {
     }
 
     public void inOrder(Node node) {
+        System.out.println("非递归中序遍历");
 //        中序遍历即左根右，意思是一直向左找，然后输出根节点，然后向右找一个再循环往复
         if (node == null) return;
         Stack<Node> stack = new Stack<Node>();
 
         while (node != null || !stack.empty()) {
+            //按照完全二叉树的形式进行中序遍历，如果节点为空就不放进栈，在这里栈保存的是中序遍历的顺序
             if (node != null) {
                 stack.push(node);
                 node = node.left;
             } else {
                 node = stack.pop();
-                System.out.println(node.value);
-                stack.push(node.right);
+                System.out.print(node.value + " ");
+                node = node.right;
             }
         }
-
+        System.out.println();
     }
 
     public void postOrder(Node node) {
+        System.out.println("非递归后序遍历");
 //        这里的思路是一个反向的先序遍历
 
         Stack<Node> stack1 = new Stack<>();
@@ -73,10 +93,8 @@ public class BinaryTreeWithoutRecursion {
         }
 
         while (!stack2.empty()) {
-            System.out.println(stack2.pop().value);
+            System.out.print(stack2.pop().value + " ");
         }
-
-
     }
 }
 
