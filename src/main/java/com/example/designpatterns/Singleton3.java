023d30a9dec21f51b516b916821bc5e2764e675f
cococolabear <794055465@qq.com>
2021-01-25 17:01:28 +0800

    增减双重校验锁的单例模式中volatile关键字的作用说明

--- src/main/java/com/example/designpatterns/Singleton3.java
+++ src/main/java/com/example/designpatterns/Singleton3.java
@@ -9,7 +9,10 @@ package com.example.designpatterns;
 //双重校验锁  安全，且在多线程下可以保证高性能
 //    双重锁是volatile和synchronized
 public class Singleton3 {
-    //    加上volatile关键字  声明变量可见性
+    //    加上volatile关键字  保证可见性和禁止指令重排序
+//    new Singleton3()是一个非原子操作（申请内存 初始化 赋值），编译器可能会进行重排序即构造函数在整个对象初始化完成前执行完毕（赋值操作在初始化对象完成前，复制操作只是在内存开出一片存储区域然后直接返回内存的引用）。
+//    此时，如果线程B在线程A赋值完但是初始化没进行完时进行判断，那么线程B将拿到一个没有初始化的半成品
+//    另外，如果线程A在自己的工作线程创建了实例，但是此时还未同步到主存中，那么此时线程B在主存中判断instance还是null，那么此时线程B将再创建一个实例，此时已经不是单例模式了
     private volatile static Singleton3 instance;
 
     public Singleton3() {
