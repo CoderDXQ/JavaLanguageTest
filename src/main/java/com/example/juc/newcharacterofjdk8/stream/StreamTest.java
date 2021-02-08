61521d3711d6d2be8cde548a6205651030e252bf
cococolabear <794055465@qq.com>
2021-02-09 00:50:52 +0800

    Stream用于计算，使用链表等进行存储

--- src/main/java/com/example/juc/newcharacterofjdk8/stream/StreamTest.java
+++ src/main/java/com/example/juc/newcharacterofjdk8/stream/StreamTest.java
@@ -5,5 +5,58 @@ package com.example.juc.newcharacterofjdk8.stream;
  * @version 1.0
  * @date 2021/2/8 11:59 下午
  */
+
+import lombok.Data;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 题目要求：一分钟内完成此题，只能用一行代码实现！
+ * 现在有5个用户！筛选：
+ * 1、ID 必须是偶数
+ * 2、年龄必须大于23岁
+ * 3、用户名转为大写字母
+ * 4、用户名字母倒着排序
+ * 5、只输出一个用户！
+ */
 public class StreamTest {
+
+    public static void main(String[] args) {
+
+        User u1 = new User(1, "a", 21);
+        User u2 = new User(2, "b", 22);
+        User u3 = new User(3, "c", 23);
+        User u4 = new User(4, "d", 24);
+        User u5 = new User(6, "e", 25);
+
+//        集合就是存储
+        List<User> list = Arrays.asList(u1, u2, u3, u4, u5);
+
+//        具体的计算交给Stream流
+        list.stream()
+                .filter(u->{return u.getId()%2==0;})
+                .filter(u->{return u.getAge()>23;})
+                .map(u->{return u.getName().toUpperCase();})
+                .sorted((uu1,uu2)->{return uu2.compareTo(uu1);})
+                .limit(1)
+                .forEach(System.out::println);
+    }
+
 }
+
+@Data
+class User {
+
+    Integer id;
+    String name;
+    Integer age;
+
+    public User(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+}
+
