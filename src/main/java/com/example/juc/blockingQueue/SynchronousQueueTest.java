8fcad621cc650a243c8180de601f86071e3eacdf
cococolabear <794055465@qq.com>
2021-02-10 23:48:14 +0800

    同步队列

--- src/main/java/com/example/juc/blockingQueue/SynchronousQueueTest.java
+++ src/main/java/com/example/juc/blockingQueue/SynchronousQueueTest.java
@@ -1,5 +1,9 @@
 package com.example.juc.blockingQueue;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -8,4 +12,43 @@ package com.example.juc.blockingQueue;
 
 //page33 page 34
 public class SynchronousQueueTest {
+    public static void main(String[] args) {
+//        同步队列 与其他的阻塞队列不一样 SynchronousQueue不存储元素，put一个元素之后必须take出来才能再次put
+        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
+
+        new Thread(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " put 1");
+                blockingQueue.put("1");
+
+                System.out.println(Thread.currentThread().getName() + " put 2");
+                blockingQueue.put("2");
+
+                System.out.println(Thread.currentThread().getName() + " put 3");
+                blockingQueue.put("3");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }, "T1").start();
+
+        new Thread(() -> {
+
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName() + "=>" + blockingQueue.take());
+
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName() + "=>" + blockingQueue.take());
+
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName() + "=>" + blockingQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }, "T2").start();
+
+    }
+
 }
