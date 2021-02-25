4792c09f7d752c1da2ac22118b41c12467651ed4
cococolabear <794055465@qq.com>
2021-02-25 22:09:42 +0800

    JZ10

---
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ10.java
@@ -0,0 +1,38 @@
+package com.example.brushalgorithmproblem.swordtooffer;
+
+import java.util.Scanner;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/2/25 10:00 下午
+ */
+public class JZ10 {
+
+    //    多算几个情况发现就是个斐波那契数列  动态规划+滚动数组优化
+    public static int rectCover(int target) {
+        int n = target;
+        int a = 1, b = 1, c = 0;
+        if (n == 1) {
+            return 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+//            方式一：
+//            b = a + b;
+//            c = b;
+//            a = c - a;//或a=b-a;
+//            方式二：
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(rectCover(in.nextInt()));
+    }
+}
