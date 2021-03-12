d8ef70406963b14b9356a791bfffd3c4197d7e0d
cococolabear <794055465@qq.com>
2021-03-13 00:22:31 +0800

    JZ45

--- src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ45.java
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ45.java
@@ -1,5 +1,7 @@
 package com.example.brushalgorithmproblem.swordtooffer;
 
+import java.util.*;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -8,6 +10,36 @@ package com.example.brushalgorithmproblem.swordtooffer;
 public class JZ45 {
 
     public static boolean IsContinuous(int[] numbers) {
+        if (numbers.length < 5) {
+            return false;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : numbers) {
+            if (map.containsKey(num)) {
+                map.replace(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+
+//        HashMap的遍历方法
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//            设置false规则 存在重复的牌且重复的牌不是大小王
+            if (entry.getValue() > 1 && entry.getKey() != 0) {
+                return false;
+            }
+            if (min > entry.getKey() && entry.getKey() != 0) {
+                min = entry.getKey();
+            }
+            if (max < entry.getKey()) {
+                max = entry.getKey();
+            }
+        }
+//        设置false规则
+        if (max - min > 4) {
+            return false;
+        }
 
         return true;
     }
