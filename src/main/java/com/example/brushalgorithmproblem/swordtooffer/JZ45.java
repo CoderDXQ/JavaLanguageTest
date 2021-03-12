277259c386178a9fc9ae05fce3c2e38c56edd542
cococolabear <794055465@qq.com>
2021-03-12 23:53:15 +0800

    add

---
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ45.java
@@ -0,0 +1,22 @@
+package com.example.brushalgorithmproblem.swordtooffer;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/12 11:51 下午
+ */
+public class JZ45 {
+
+    public static boolean IsContinuous(int[] numbers) {
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = new int[]{0, 3, 2, 6, 4};
+
+//        true
+        System.out.println(IsContinuous(numbers));
+
+    }
+}
