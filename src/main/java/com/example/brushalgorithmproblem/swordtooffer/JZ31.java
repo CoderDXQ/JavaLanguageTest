0dce282f7ec24573a02c9f3fb2b2c3696d4dca7c
cococolabear <794055465@qq.com>
2021-03-12 23:46:42 +0800

    add

---
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ31.java
@@ -0,0 +1,23 @@
+package com.example.brushalgorithmproblem.swordtooffer;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/12 11:43 下午
+ */
+public class JZ31 {
+
+    public static int NumberOf1Between1AndN_Solution(int n) {
+        int sum = 0;
+
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        int n = 13;
+        System.out.println();
+        NumberOf1Between1AndN_Solution(n);
+    }
+}
---
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ35.java
@@ -0,0 +1,24 @@
+package com.example.brushalgorithmproblem.swordtooffer;
+
+import java.lang.reflect.InvocationHandler;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/12 11:44 下午
+ */
+public class JZ35 {
+
+    public static int InversePairs(int[] array) {
+        int sum = 0;
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 0};
+//        7
+        System.out.println(InversePairs(array));
+    }
+}
