1059e85a7b5d29439df40897291853c447a7d342
cococolabear <794055465@qq.com>
2021-03-13 23:03:36 +0800

    JZ57

--- src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ57.java
+++ src/main/java/com/example/brushalgorithmproblem/swordtooffer/JZ57.java
@@ -11,6 +11,7 @@ public class JZ57 {
         int val;
         TreeLinkNode left = null;
         TreeLinkNode right = null;
+        //        存放的是当前节点的父节点指针
         TreeLinkNode next = null;
 
         TreeLinkNode(int val) {
@@ -19,9 +20,29 @@ public class JZ57 {
     }
 
     public static TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode == null) {
+            return pNode;
+        }
 
+//        如果当前节点有右子树(左子树已经被访问过 不需要考虑  肯定不是下一节点)那么下一节点就是右子树的最左节点
+        if (pNode.right != null) {
+            pNode = pNode.right;
+            while (pNode.left != null) {
+                pNode = pNode.left;
+            }
+            return pNode;
+        }
 
-        return pNode;
+//        如果当前节点没有右子树 需要向上查找
+//        这样的话  如果当前节点是父节点的右节点 继续需要向上查找
+//        如果当前节点是父节点的左节点,那么下一个节点就是父节点
+        while (pNode.next != null) {
+            if (pNode.next.left == pNode) {
+                return pNode.next;
+            }
+            pNode = pNode.next;
+        }
+        return null;
     }
 
 
