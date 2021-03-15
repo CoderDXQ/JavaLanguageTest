0a5f3af2d4f6d1bfbe939c9db3c9d0eaaf010760
cococolabear <794055465@qq.com>
2021-03-16 02:11:55 +0800

    lt283

---
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt283.java
@@ -0,0 +1,36 @@
+package com.example.brushalgorithmproblem.leetcodehot100;
+
+import java.util.Arrays;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/16 1:07 上午
+ */
+public class lt283 {
+
+    //    直接覆盖
+    public static void moveZeroes(int[] nums) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
+        }
+        while (k < nums.length) {
+            nums[k++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        Arrays.stream(nums).forEach(a -> {
+            System.out.print(a + " ");
+        });
+        System.out.println();
+
+    }
+}
