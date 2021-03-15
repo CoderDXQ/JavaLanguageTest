0d18ca379f5b03d0d5e295265300183ff2e567d4
cococolabear <794055465@qq.com>
2021-03-16 02:19:31 +0800

    lt226

---
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt226.java
@@ -0,0 +1,28 @@
+package com.example.brushalgorithmproblem.leetcodehot100;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/16 2:14 上午
+ */
+public class lt226 {
+
+    //    改进先序遍历
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] tree = new int[]{4, 2, 7, 1, 3, 6, 9};
+        BinaryTreeUtils.layerOrder(invertTree(BinaryTreeUtils.create1(tree)));
+    }
+}
