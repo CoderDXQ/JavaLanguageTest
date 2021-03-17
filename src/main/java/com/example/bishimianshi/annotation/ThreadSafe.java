7880603c2cc87bf0f0f04c475d608a37de035e67
cococolabear <794055465@qq.com>
2021-03-17 23:22:55 +0800

    自定义注解

---
+++ src/main/java/com/example/bishimianshi/annotation/ThreadSafe.java
@@ -0,0 +1,18 @@
+package com.example.bishimianshi.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义线程安全的注解
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/17 11:21 下午
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ThreadSafe {
+    String value();
+}
