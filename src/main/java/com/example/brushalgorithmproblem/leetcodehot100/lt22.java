de6827c2261c3a6054d0f97ec6775c53f7bdcc23
cococolabear <794055465@qq.com>
2021-03-17 23:51:09 +0800

    add

---
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt22.java
@@ -0,0 +1,20 @@
+package com.example.brushalgorithmproblem.leetcodehot100;
+
+import java.util.List;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/17 11:49 下午
+ */
+
+//括号生成
+public class lt22 {
+    public static List<String> generateParenthesis(int n) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
