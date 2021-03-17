17c9d855a707de1178d52910bff9489f6b5e5ce4
cococolabear <794055465@qq.com>
2021-03-17 22:47:37 +0800

    lt152

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt152.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt152.java
@@ -1,5 +1,7 @@
 package com.example.brushalgorithmproblem.leetcodehot100;
 
+import com.example.datastructure.sort.QuickSort;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -12,8 +14,21 @@ public class lt152 {
     //    动态规划
     public static int maxProduct(int[] nums) {
         int result = Integer.MIN_VALUE;
-
-
+//        进行乘法运算就要赋值为1
+        int min = 1;
+        int max = 1;
+//        因为存在正负值的情况 所以要维护最大和最小两个值(维护绝对值最大的情况)
+        for (int n : nums) {
+//            这一步可能造成后来的max小于之前的max 所以需要result
+            if (n < 0) {
+                int temp = min;
+                min = max;
+                max = temp;
+            }
+            min = Math.min(n, n * min);
+            max = Math.max(n, n * max);
+            result = Math.max(result, max);
+        }
         return result;
     }
 
@@ -35,5 +50,6 @@ public class lt152 {
     public static void main(String[] args) {
         int[] nums = new int[]{2, 3, -2, 4};
         System.out.println(maxProduct(nums));
+        System.out.println(maxProduct1(nums));
     }
 }
