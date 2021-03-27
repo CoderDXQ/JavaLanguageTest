4a2d75c67d2b0fd818ddae037b415889569fe97c
cococolabear <794055465@qq.com>
2021-03-27 22:55:46 +0800

    lt137位运算

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt137.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt137.java
@@ -1,5 +1,8 @@
 package com.example.brushalgorithmproblem.leetcodehot100;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -7,13 +10,65 @@ package com.example.brushalgorithmproblem.leetcodehot100;
  */
 public class lt137 {
 
-    public int singleNumber(int[] nums) {
-
-        return 0;
+    //    int是32位的
+    public static int singleNumber(int[] nums) {
+//        处理每一位
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int sumOfBit = 0;
+            for (int n : nums) {
+//                无符号右移 注意优先级
+                if (((n >>> i) & 1) == 1) {
+                    sumOfBit++;
+                }
+            }
+            if (sumOfBit % 3 == 1) {
+                result = (1 << i) | result;
+            }
+        }
+        return result;
     }
 
-    public static void main(String[] args) {
+    public static int singleNumber1(int[] nums) {
+//        处理每一位
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int sumOfBit = 0;
+            for (int n = 0; n < nums.length; n++) {
+//                无符号右移 注意优先级
+                if ((nums[n] & 1) == 1) {
+                    sumOfBit++;
+                }
+                nums[n] >>>= 1;
+            }
+            if (sumOfBit % 3 == 1) {
+                result = (1 << i) | result;
+            }
+        }
+        return result;
+    }
 
+    //    算数方法
+    public static int singleNumber2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        long sum = 0;
+        for (int n : nums) {
+            sum += n;
+            set.add(n);
+        }
+        long sum1 = 0;
+        for (int n : set) {
+            sum1 += n;
+        }
+        return (int) ((sum1 * 3 - sum) >> 1);
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 1, 0, 1, 99};
+        System.out.println(singleNumber(nums));
+        System.out.println(singleNumber1(nums));
+        System.out.println(singleNumber2(nums));
     }
 
 }
