217546dae7dfcb93405c3c6962da6fb2e08fc195
cococolabear <794055465@qq.com>
2021-03-27 22:35:04 +0800

    lt200并查集

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt200.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt200.java
@@ -97,11 +97,102 @@ public class lt200 {
         return result;
     }
 
+    //    并查集
+    static class UnionFind {
+        //        连通块数量
+        int count;
+        //parent数组中存放的是下标的爹
+        int[] parent;
+        int[] rank;
+
+        //        构造函数
+        public UnionFind(char[][] grid) {
+//            地块数量
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+//                        并查集初始化  此时每个点的父亲都是自己
+                        parent[i * n + j] = i * n + j;
+                        count++;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) {
+//            只有所有人的爹才能给自己当爹
+            if (parent[i] != i) {
+                parent[i] = find(parent[i]);
+            }
+            return parent[i];
+//            三目运算符写法
+//           return parent[i] == i ? parent[i] : find(parent[i]);
+        }
+
+        public void union(int x, int y) {
+            int rootx = find(x);
+            int rooty = find(y);
+//            不在同一个连通块里 此时需要合并
+            if (rootx != rooty) {
+//                大的当爹
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx;
+//                    设置爹的rank值
+                    rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
     //    并查集  连通块问题就用并查集
     public static int numIslands2(char[][] grid) {
 
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
 
-        return 0;
+        int nr = grid.length;
+        int nc = grid[0].length;
+//        初始化并查集
+        UnionFind uf = new UnionFind(grid);
+
+        for (int r = 0; r < nr; r++) {
+            for (int c = 0; c < nc; c++) {
+                if (grid[r][c] == '1') {
+//                    miu掉
+                    grid[r][c] = '0';
+//                    把某个块周围的地块连接到这个块上
+                    if (r - 1 >= 0 && grid[r - 1][c] == '1') {
+                        uf.union(r * nc + c, (r - 1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r + 1][c] == '1') {
+                        uf.union(r * nc + c, (r + 1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c - 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c + 1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+        return uf.getCount();
     }
 
     public static void main(String[] args) {
@@ -121,8 +212,8 @@ public class lt200 {
         }
 
 //        System.out.println(numIslands(grid));
-        System.out.println(numIslands1(grid));
-//        System.out.println(numIslands2(grid));
+//        System.out.println(numIslands1(grid));
+        System.out.println(numIslands2(grid));
 
     }
 }
