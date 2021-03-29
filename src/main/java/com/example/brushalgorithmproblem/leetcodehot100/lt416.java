da25929ff33216fd08167132628e00d11073b9be
cococolabear <794055465@qq.com>
2021-03-29 23:54:58 +0800

    lt416

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt416.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt416.java
@@ -8,12 +8,87 @@ package com.example.brushalgorithmproblem.leetcodehot100;
 //分割等和子集
 public class lt416 {
 
+    //    动态规划 0-1背包问题
     public static boolean canPartition(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int i : nums) {
+            sum += i;
+            maxNum = Math.max(maxNum, i);
+        }
 
-        return true;
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        int target = sum >> 1;
+        if (maxNum > target) {
+            return false;
+        }
+
+//        第一个坐标是物品编号  第二个坐标是背包容量
+        boolean[][] dp = new boolean[n][target + 1];
+//        初始化
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+
+//        初始化
+        dp[0][nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = 1; j <= target; j++) {
+                if (j >= num) {
+//                         不放这个物品且容量刚好   放这个物品且容量刚好
+                    dp[i][j] = dp[i - 1][j] | dp[i - 1][j - num];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n - 1][target];
+    }
+
+    //    思路优化
+    public static boolean canPartition1(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = 0, maxNum = 0;
+        for (int i : nums) {
+            sum += i;
+            maxNum = Math.max(maxNum, i);
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+
+        int target = sum >> 1;
+        if (maxNum > target) {
+            return false;
+        }
+
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] |= dp[j - nums[i]];
+            }
+        }
+
+        return dp[target];
     }
 
     public static void main(String[] args) {
 
+        int[] nums = new int[]{1, 5, 11, 5};
+        System.out.println(canPartition(nums));
+        System.out.println(canPartition1(nums));
     }
 }
