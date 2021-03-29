a9b629a25624d3f2df90c4b001291ac0c6ad115b
cococolabear <794055465@qq.com>
2021-03-29 22:36:32 +0800

    lt300

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt300.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt300.java
@@ -11,15 +11,57 @@ public class lt300 {
 
     //    动态规划
     public static int lengthOfLIS(int[] nums) {
-
-        return 0;
+        int result = 1;
+//        记录在某个下标的最长递增子序列的长度
+        int[] dp = new int[nums.length];
+//        初始化
+        dp[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+//            初始化
+            dp[i] = 1;
+//            从头开始比较 保证无后效性
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+        return result;
     }
 
 
-    //    二分查找
+    //  贪心+二分查找
     public static int lengthOfLIS1(int[] nums) {
-
-        return 0;
+        int len = 1, n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] d = new int[n + 1];
+        d[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len]) {
+//                数组d就是最长递增子序列
+                d[++len] = nums[i];
+            } else {
+//                二分查找
+                int l = 1, r = len, pos = 0;
+                while (l <= r) {
+//                    这样pos最后的值就是最接近nums[i]但是小于nums[i]的值
+                    int mid = (l + r) >> 1;
+                    if (d[mid] < nums[i]) {
+                        l = mid + 1;
+                        pos = mid;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+//                这个更新没有后效性
+//                更新最长递增子序列
+                d[pos + 1] = nums[i];
+            }
+        }
+        return len;
     }
 
 
---
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt338.java
@@ -0,0 +1,15 @@
+package com.example.brushalgorithmproblem.leetcodehot100;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/3/29 9:56 下午
+ */
+public class lt338 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
