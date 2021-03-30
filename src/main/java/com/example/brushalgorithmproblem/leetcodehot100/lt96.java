4fa7501f5b14c569408bcc59874edf5910f95562
cococolabear <794055465@qq.com>
2021-03-30 12:17:49 +0800

    lt96

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt96.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt96.java
@@ -7,10 +7,18 @@ package com.example.brushalgorithmproblem.leetcodehot100;
  */
 public class lt96 {
 
+    //    本质上是一个数学问题
+//    任选一个结点作为根节点 此时的种类数为左子树的种类数乘以右子树的种类数
     public static int numTrees(int n) {
-
-
-        return n;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += (dp[j - 1] * dp[i - j]);
+            }
+        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
