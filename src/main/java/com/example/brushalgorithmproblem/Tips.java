ed5b50f96927eb03ab48b0842c53d3599a866843
cococolabear <794055465@qq.com>
2021-03-30 12:42:34 +0800

    lt56

--- src/main/java/com/example/brushalgorithmproblem/Tips.java
+++ src/main/java/com/example/brushalgorithmproblem/Tips.java
@@ -1,5 +1,7 @@
 package com.example.brushalgorithmproblem;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -18,6 +20,17 @@ public class Tips {
 //        基本类型数组转包装类型数组
         int[] jilu = new int[]{};
         Integer[] jisuan = IntStream.of(jilu).boxed().collect(Collectors.toList()).toArray(new Integer[0]);
+
+//        二维数组排序
+        int[][] intervals = new int[][]{{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                Integer i1 = o1[0];
+                Integer i2 = o2[0];
+                return i1.compareTo(i2);
+            }
+        });
 
     }
 
--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt56.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt56.java
@@ -1,5 +1,10 @@
 package com.example.brushalgorithmproblem.leetcodehot100;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -7,16 +12,36 @@ package com.example.brushalgorithmproblem.leetcodehot100;
  */
 public class lt56 {
 
+    //    常规方法
     public static int[][] merge(int[][] intervals) {
 
+//        二维数组排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
 
-        return intervals;
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 0; i < intervals.length; i++) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            if (i == 0 || merged.get(merged.size() - 1)[1] < left) {
+                merged.add(new int[]{left, right});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(right, merged.get(merged.size() - 1)[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
     }
 
 
     public static void main(String[] args) {
 
-        int[][] intervals = new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] intervals = new int[][]{{1, 3}, {8, 10}, {2, 6}, {15, 18}};
         for (int[] a : merge(intervals)) {
             for (int b : a) {
                 System.out.print(b + " ");
