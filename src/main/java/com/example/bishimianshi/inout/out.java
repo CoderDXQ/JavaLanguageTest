1d78f4a9f641d62d1baffa6d2a928657ac477586
cococolabear <794055465@qq.com>
2021-04-03 21:56:50 +0800

    java使用printf进行标准输出

--- src/main/java/com/example/bishimianshi/inout/out.java
+++ src/main/java/com/example/bishimianshi/inout/out.java
@@ -42,6 +42,7 @@ public class out {
         System.out.printf("%16.3f\n", 5.5);
 //        左对齐 小数点后填充补足3位即可
         System.out.printf("%-16.3f\n", 5.5);
+        System.out.printf("%f; %f;%n", -756.403f, 7464.232641d);
 
         System.out.println();
 
@@ -62,6 +63,36 @@ public class out {
         char chTemp = 'a';
         System.out.printf("%s%c:\n十进制:%d\n十六进制:%x\n", "输出字符型变量", chTemp, (int) chTemp, (int) chTemp);
         System.out.println();
+
+        System.out.println("科学记数法：");
+        System.out.printf("%e\n", -45.5677f);
+        System.out.printf("%e\n", -45.5677d);
+//        大写
+        System.out.printf("%E\n", -45.5677f);
+        System.out.printf("%E\n", -45.5677d);
+        System.out.println();
+
+        System.out.println("字符串相关：");
+//        转换为大写
+        System.out.printf("%S\n", "asd");
+        System.out.println();
+
+        System.out.println("boolean类型相关：");
+        System.out.printf("true=%b\n", true);
+        System.out.println();
+
+        System.out.println("整数相关：");
+//        十进制
+        System.out.printf("%d; %d; %n", -500, 2343L);
+//        八进制
+        System.out.printf("%o; %o; %n", -500, 2343L);
+//        十六进制
+        System.out.printf("%x; %x; %n", -500, 2343L);
+//        十六进制大写
+        System.out.printf("%X; %X; %n", -500, 2343L);
+
+        System.out.println();
+
 
     }
 
