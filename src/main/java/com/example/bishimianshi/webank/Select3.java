1b6995280b3f30502e5ab03e01aa8a8403f09dad
cococolabear <794055465@qq.com>
2021-04-06 22:00:33 +0800

    WeBank选择题

---
+++ src/main/java/com/example/bishimianshi/webank/Select3.java
@@ -0,0 +1,19 @@
+package com.example.bishimianshi.webank;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/4/6 9:07 下午
+ */
+public class Select3 {
+
+    //    private static final int counter = 10;
+//    final在构造函数之后加了一个StoreStore屏障 后面的不能再写了
+    private static int counter = 10;
+
+    public static void main(String[] args) {
+//        因为有final修饰 编译时出错
+        System.out.println(++counter);
+    }
+
+}
