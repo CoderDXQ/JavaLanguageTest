763ba331a4921d374effa055c7c61f1d24d59768
cococolabear <794055465@qq.com>
2021-04-09 23:50:33 +0800

    线程同步工具

--- src/main/java/com/example/writtenexaminationandinterview/synchronizers/CountDownLatchTest.java
+++ src/main/java/com/example/writtenexaminationandinterview/synchronizers/CountDownLatchTest.java
@@ -1,5 +1,7 @@
 package com.example.writtenexaminationandinterview.synchronizers;
 
+import java.util.concurrent.*;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -7,8 +9,86 @@ package com.example.writtenexaminationandinterview.synchronizers;
  */
 public class CountDownLatchTest {
 
+    static class Worker implements Callable<Integer> {
+        private CountDownLatch latch;
+        private int start;
+        private int end;
 
-    public static void main(String[] args) {
+        public Worker(CountDownLatch latch, int start, int end) {
+            this.latch = latch;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public Integer call() throws Exception {
+            int count = 0;
+            for (int i = start; i <= end; i++) {
+
+                if (isPrime(i)) {
+                    count++;
+                }
+            }
+            latch.countDown();
+            return count;
+        }
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % 1 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+//        Cpu数目  cpu密集型
+        int cpus = Runtime.getRuntime().availableProcessors() + 1;
+        CountDownLatch countDownLatch = new CountDownLatch(cpus);
+        ExecutorService executors = Executors.newFixedThreadPool(cpus);
+
+        int bound = 10000000;
+        int start = 1, end = 1, step = bound / cpus;
+        int count = 0;
+        long multiStart = System.currentTimeMillis();
+
+//        用CompletionService包装 方便通过take从blockingQueue中取
+        CompletionService<Integer> service = new ExecutorCompletionService<>(executors);
+
+        for (int i = 0; i < cpus; i++) {
+            end = Math.min(bound, start + step);
+            service.submit(new Worker(countDownLatch, start, end - 1));
+            start = end + 1;
+        }
+
+//        等待所有线程执行完毕
+        countDownLatch.await();
+
+//        合并计算结果
+        for (int i = 0; i < cpus; i++) {
+            count += service.take().get();
+        }
+        System.out.println("multi : " + count);
+        long multiEnd = System.currentTimeMillis();
+        System.out.println("multi : " + (multiEnd - multiStart));
+//        关闭线程池
+        executors.shutdown();
+
+        int c = 0;
+        long singleStart = System.currentTimeMillis();
+        for (int i = 1; i <= bound; i++) {
+            if (isPrime(i)) {
+                c++;
+            }
+        }
+        System.out.println("single : " + c);
+        long singleEnd = System.currentTimeMillis();
+        System.out.println("single time : " + (singleEnd - singleStart));
+
 
     }
 
--- src/main/java/com/example/writtenexaminationandinterview/synchronizers/CyclicBarrierTest.java
+++ src/main/java/com/example/writtenexaminationandinterview/synchronizers/CyclicBarrierTest.java
@@ -1,6 +1,13 @@
 package com.example.writtenexaminationandinterview.synchronizers;
 
-import java.util.concurrent.Callable;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 /**
  * CountDownLatch实现并行计算
@@ -11,16 +18,65 @@ import java.util.concurrent.Callable;
  */
 public class CyclicBarrierTest {
 
-    static class Worker implements Callable<Integer> {
+    static class Student implements Runnable {
+        private CyclicBarrier barrier;
+        private String name;
+
+        public Student(CyclicBarrier barrier, String name) {
+            this.barrier = barrier;
+            this.name = name;
+        }
 
         @Override
-        public Integer call() throws Exception {
-            return null;
+        public void run() {
+
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+                Random random = new Random();
+
+                Thread.sleep(random.nextInt(3000));
+                System.out.println(sdf.format(new Date()) + " " + name + "出发去饭店");
+                Thread.sleep(random.nextInt(3000));
+                System.out.println(sdf.format(new Date()) + " " + name + "到了饭店");
+
+//                等到barrier增加到设定的数值之后再向下执行
+                barrier.await();
+//                barrier.wait();
+
+                System.out.println(sdf.format(new Date()) + " " + name + "开始吃饭");
+                Thread.sleep(random.nextInt(3000));
+                System.out.println(sdf.format(new Date()) + " " + name + "吃完了");
+
+//            重用
+                barrier.await();
+//                barrier.wait();
+
+                System.out.println(sdf.format(new Date()) + " " + name + "离开餐厅");
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        CyclicBarrier barrier = new CyclicBarrier(3);
+
+        List<Thread> threads = new ArrayList<>(3);
+        threads.add(new Thread(new Student(barrier, "A")));
+        threads.add(new Thread(new Student(barrier, "B")));
+        threads.add(new Thread(new Student(barrier, "C")));
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
 
     }
 
