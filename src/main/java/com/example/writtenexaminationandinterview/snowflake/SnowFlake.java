344f5ee93baa4d14f2d67c54bbd92f5e8b8f63c3
cococolabear <794055465@qq.com>
2021-04-11 14:46:16 +0800

    好未来笔试

---
+++ src/main/java/com/example/writtenexaminationandinterview/snowflake/SnowFlake.java
@@ -0,0 +1,11 @@
+package com.example.writtenexaminationandinterview.snowflake;
+
+/**
+ *
+ * 雪花算法  用于生成全局唯一id
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/4/11 12:21 下午
+ */
+public class SnowFlake {
+}
