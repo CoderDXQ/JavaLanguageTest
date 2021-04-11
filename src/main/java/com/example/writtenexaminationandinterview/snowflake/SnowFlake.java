61cc234a82ca262234241605f148b0747f4ee109
cococolabear <794055465@qq.com>
2021-04-11 17:24:29 +0800

    度小满笔试

--- src/main/java/com/example/writtenexaminationandinterview/snowflake/SnowFlake.java
+++ src/main/java/com/example/writtenexaminationandinterview/snowflake/SnowFlake.java
@@ -8,4 +8,41 @@ package com.example.writtenexaminationandinterview.snowflake;
  * @date 2021/4/11 12:21 下午
  */
 public class SnowFlake {
+
+//    下面是10位的工作机器id
+    private long workerId;
+    private long datacenterId;
+
+//    12位的序列号
+    private long sequence;
+
+    public void IdWorker(long workerId, long datacenterId, long sequence){
+
+
+
+
+    }
+
+//    初始时间戳
+    private long twepoch=1288834974657L;
+
+//    机房  机器id 长度是5位
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
 }
