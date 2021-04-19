18ec67fe4edb0e9efb3e77ff4c03715e39e030be
cococolabear <794055465@qq.com>
2021-04-19 23:29:42 +0800

    手写IOC

---
+++ src/main/java/com/example/writtenexaminationandinterview/myIOC/Test.java
@@ -0,0 +1,21 @@
+package com.example.writtenexaminationandinterview.myIOC;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/4/19 10:30 下午
+ */
+public class Test {
+
+    @org.junit.Test
+    public void getBean() throws Exception {
+//        不能有汉字
+        String location = SimpleIOC.class.getClassLoader().getResource("ioc.xml").getFile();
+        SimpleIOC bf = new SimpleIOC(location);
+        Wheel wheel = (Wheel) bf.getBean("wheel");
+        System.out.println(wheel);
+        Car car = (Car) bf.getBean("car");
+        System.out.println(car);
+    }
+
+}
---
+++ src/main/java/com/example/writtenexaminationandinterview/myIOC/Wheel.java
@@ -0,0 +1,37 @@
+package com.example.writtenexaminationandinterview.myIOC;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/4/19 10:30 下午
+ */
+public class Wheel {
+
+    private String brand;
+    private String specification;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheel{" +
+                "brand='" + brand + '\'' +
+                ", specification='" + specification + '\'' +
+                '}';
+    }
+
+}
