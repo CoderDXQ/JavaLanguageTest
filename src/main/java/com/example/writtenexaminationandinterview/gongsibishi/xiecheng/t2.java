e36554ef891c0c977eb17f22f7209c0891ce77e5
cococolabear <794055465@qq.com>
2021-04-25 22:12:38 +0800

    建房问题

--- src/main/java/com/example/writtenexaminationandinterview/gongsibishi/xiecheng/t2.java
+++ src/main/java/com/example/writtenexaminationandinterview/gongsibishi/xiecheng/t2.java
@@ -1,5 +1,7 @@
 package com.example.writtenexaminationandinterview.gongsibishi.xiecheng;
 
+import java.util.Scanner;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -13,6 +15,25 @@ public class t2 {
 //    可以考虑反着来建立映射关系
 
     public static void main(String[] args) {
-        System.out.println(-1);
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String result = "R";
+        while (n > 1) {
+            String s = "";
+            for (int i = 0; i < result.length(); i++) {
+                if ((i & 1) == 0) {
+                    s += "G";
+                    s += result.charAt(i);
+                    s += "R";
+                } else {
+                    s += result.charAt(i);
+                }
+            }
+            result = s;
+            n--;
+        }
+
+        System.out.println(result);
     }
 }
