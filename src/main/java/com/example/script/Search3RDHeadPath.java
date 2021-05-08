b77c172de3a5c6bc769be7f4f77d4d17199c8751
duanxiangqing <794055465@qq.com>
2021-05-08 17:02:19 +0800

    a

---
+++ src/main/java/com/example/script/Search3RDHeadPath.java
@@ -0,0 +1,29 @@
+package com.example.script;
+
+import java.util.List;
+
+/**
+ * @author duanxiangqing
+ * @date 2021/5/8
+ */
+public class Search3RDHeadPath {
+
+
+
+    public static List<String> Search3RDHeadPathByARGS(String path) {
+
+        //从path变量读文件 需要使用set去重
+
+        //递归遍历上一步得到的数据 需要使用set去重
+
+        //
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
