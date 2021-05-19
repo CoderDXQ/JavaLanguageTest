983bc17ff0b63e684e4b820d4dddeb3420a86b3e
duanxiangqing <794055465@qq.com>
2021-05-19 11:31:52 +0800

    lt128并查集

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt128.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt128.java
@@ -1,5 +1,8 @@
 package com.example.brushalgorithmproblem.leetcodehot100;
 
+import java.net.Inet4Address;
+import java.util.*;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
@@ -8,10 +11,91 @@ package com.example.brushalgorithmproblem.leetcodehot100;
 //最长连续序列
 public class lt128 {
 
+    //    哈希表
     public static int longestConsecutive(int[] nums) {
 
+        Set<Integer> set = new HashSet<>();
 
-        return 0;
+        for (int i : nums) {
+            set.add(i);
+        }
+
+        int max = 0;
+
+        for (int num : set) {
+
+            if (!set.contains(num - 1)) {
+                int cur = num;
+                int count = 1;
+
+                while (set.contains(cur + 1)) {
+                    cur++;
+                    count++;
+                }
+
+                max = Math.max(max, count);
+
+            }
+
+        }
+
+        return max;
+    }
+
+    //    排序 时间复杂度取决于Arrays.sort(nums);
+    public static int longestConsecutive1(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int res = 0;
+
+//        排序
+        Arrays.sort(nums);
+
+        int count = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1] - 1) {
+                count++;
+            } else if ((nums[i] == nums[i + 1])) {
+            } else {
+                count = 1;
+            }
+            res = Math.max(res, count);
+        }
+
+        res = Math.max(res, count);
+        return res;
+    }
+
+
+    public static Map<Integer, Integer> map;
+
+    //    并查集 巧妙地利用了找爹函数的特性
+    public static int longestConsecutive2(int[] nums) {
+
+//        初始化
+        map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, i + 1);
+        }
+
+        int ans = 0;
+
+        for (int i : nums) {
+            int y = find(i + 1);
+            ans = Math.max(ans, y - i);
+        }
+
+        return ans;
+    }
+
+    public static Integer find(int x) {
+//        return map.containsKey(x) ? map.put(x, find(map.get(x))) : x;
+        return map.containsKey(x) ? find(map.get(x)) : x;
+
     }
 
 
@@ -22,6 +106,16 @@ public class lt128 {
 
         System.out.println(longestConsecutive(nums));
         System.out.println(longestConsecutive(nums1));
+
+        System.out.println();
+
+        System.out.println(longestConsecutive1(nums));
+        System.out.println(longestConsecutive1(nums1));
+
+        System.out.println();
+
+        System.out.println(longestConsecutive2(nums));
+        System.out.println(longestConsecutive2(nums1));
 
 
     }
