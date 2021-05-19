6c781d248e22dcba0aac952958a289d1a906f5bd
duanxiangqing <794055465@qq.com>
2021-05-19 15:49:18 +0800

    add

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt239.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt239.java
@@ -9,8 +9,40 @@ package com.example.brushalgorithmproblem.leetcodehot100;
 //滑动窗口最大值
 public class lt239 {
 
+    //    优先队列
+    public static int[] maxSlidingWindow(int[] nums, int k) {
 
+        return null;
+    }
+
+    //    单调队列
+    public static int[] maxSlidingWindow1(int[] nums, int k) {
+
+        return null;
+    }
+
+    //    双向队列
+    public static int[] maxSlidingWindow2(int[] nums, int k) {
+
+        return null;
+    }
+
+    public static int[] maxSlidingWindow3(int[] nums, int k) {
+
+        return null;
+    }
+
+    //    分块+预处理 Sparse Table  ST算法
     public static void main(String[] args) {
+
+        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3;
+
+        System.out.println(maxSlidingWindow(nums, k));
+        System.out.println(maxSlidingWindow1(nums, k));
+        System.out.println(maxSlidingWindow2(nums, k));
+        System.out.println(maxSlidingWindow3(nums, k));
+
 
     }
 
---
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt72.java
@@ -0,0 +1,43 @@
+package com.example.brushalgorithmproblem.leetcodehot100;
+
+/**
+ * @author duanxiangqing
+ * @date 2021/5/19
+ */
+public class lt72 {
+
+    //    记忆化搜索
+    public static int minDistance(String word1, String word2) {
+
+
+        return 0;
+    }
+
+    //    动态规划
+    public static int minDistance1(String word1, String word2) {
+
+
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+
+        String word1 = "horse";
+        String word2 = "ros";
+
+        String word11 = "intention";
+        String word22 = "execution";
+
+        System.out.println(minDistance(word1, word2));
+        System.out.println(minDistance(word11, word22));
+
+        System.out.println();
+
+        System.out.println(minDistance1(word1, word2));
+        System.out.println(minDistance1(word11, word22));
+
+
+    }
+
+}
