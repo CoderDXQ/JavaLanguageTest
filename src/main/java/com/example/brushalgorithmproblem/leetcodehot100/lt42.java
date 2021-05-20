ae150da1db6528cb24dfbe52f9655f28dc626074
duanxiangqing <794055465@qq.com>
2021-05-20 20:03:01 +0800

    lt42

--- src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt42.java
+++ src/main/java/com/example/brushalgorithmproblem/leetcodehot100/lt42.java
@@ -26,16 +26,77 @@ public class lt42 {
         return 0;
     }
 
-    //    按高度走
+    //    按高度走 按行求
     public static int trap3(int[] height) {
 
-        return 0;
+        int ans = 0;
+
+        int max = getMax(height);
+
+//        枚举高度
+        for (int i = 1; i <= max; i++) {
+
+//            信号量  标志是否开始计算
+            boolean isStart = false;
+
+            int temp_sum = 0;
+
+//          横向遍历
+            for (int j = 0; j < height.length; j++) {
+
+//                统计这个坑在这个高度山的面积
+                if (isStart && height[j] < i) {
+                    temp_sum++;
+                }
+
+//                遇到了坑的边缘
+                if (height[j] >= i) {
+                    ans += temp_sum;
+                    temp_sum = 0;
+                    isStart = true;
+                }
+            }
+        }
+        return ans;
     }
 
-    //    从左到右走
+    public static int getMax(int[] height) {
+        int max = 0;
+        for (int i = 0; i < height.length; i++) {
+            if (height[i] > max) {
+                max = height[i];
+            }
+        }
+        return max;
+    }
+
+    //    从左到右走 按列求
     public static int trap4(int[] height) {
 
-        return 0;
+        int ans = 0;
+
+        int n = height.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int maxLeft = 0, maxRight = 0;
+
+//            寻找当前的坑的左边界
+            for (int j = i; j >= 0; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+
+//            寻找当前的坑的右边界
+            for (int j = i; j < n; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+
+//            每次只加上当前的水柱
+            ans += Math.max(maxLeft, maxRight) - height[i];
+
+        }
+
+        return ans;
     }
 
 
