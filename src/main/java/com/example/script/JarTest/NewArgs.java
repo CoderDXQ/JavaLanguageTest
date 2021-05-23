37a1df16103ec976c9b0ad375c6b7fe0a14c05eb
cococolabear <794055465@qq.com>
2021-05-23 15:19:57 +0800

    DTS新的参数解析

---
+++ src/main/java/com/example/script/JarTest/NewArgs.java
@@ -0,0 +1,199 @@
+package com.example.script.JarTest;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/5/23 12:37 下午
+ */
+
+//新的参数设置  通过这个类在不同的业务之间实现传输
+public class NewArgs {
+
+    public static String args0;
+    public static String args1;
+    public static String args2;
+    public static String args3;
+    public static String args4;
+    public static String args5;
+    public static String args6;
+    public static String args7;
+    public static String args8;
+    public static String args9;
+    public static String args10;
+    public static String args11;
+    public static String args12;
+    public static String args13;
+    public static String args14;
+
+    public static String getArgs0() {
+        return args0;
+    }
+
+    public static void setArgs0(String args0) {
+        NewArgs.args0 = args0;
+    }
+
+    public static String getArgs1() {
+        return args1;
+    }
+
+    public static void setArgs1(String args1) {
+        NewArgs.args1 = args1;
+    }
+
+    public static String getArgs2() {
+        return args2;
+    }
+
+    public static void setArgs2(String args2) {
+        NewArgs.args2 = args2;
+    }
+
+    public static String getArgs3() {
+        return args3;
+    }
+
+    public static void setArgs3(String args3) {
+        NewArgs.args3 = args3;
+    }
+
+    public static String getArgs4() {
+        return args4;
+    }
+
+    public static void setArgs4(String args4) {
+        NewArgs.args4 = args4;
+    }
+
+    public static String getArgs5() {
+        return args5;
+    }
+
+    public static void setArgs5(String args5) {
+        NewArgs.args5 = args5;
+    }
+
+    public static String getArgs6() {
+        return args6;
+    }
+
+    public static void setArgs6(String args6) {
+        NewArgs.args6 = args6;
+    }
+
+    public static String getArgs7() {
+        return args7;
+    }
+
+    public static void setArgs7(String args7) {
+        NewArgs.args7 = args7;
+    }
+
+    public static String getArgs8() {
+        return args8;
+    }
+
+    public static void setArgs8(String args8) {
+        NewArgs.args8 = args8;
+    }
+
+    public static String getArgs9() {
+        return args9;
+    }
+
+    public static void setArgs9(String args9) {
+        NewArgs.args9 = args9;
+    }
+
+    public static String getArgs10() {
+        return args10;
+    }
+
+    public static void setArgs10(String args10) {
+        NewArgs.args10 = args10;
+    }
+
+    public static String getArgs11() {
+        return args11;
+    }
+
+    public static void setArgs11(String args11) {
+        NewArgs.args11 = args11;
+    }
+
+    public static String getArgs12() {
+        return args12;
+    }
+
+    public static void setArgs12(String args12) {
+        NewArgs.args12 = args12;
+    }
+
+    public static String getArgs13() {
+        return args13;
+    }
+
+    public static void setArgs13(String args13) {
+        NewArgs.args13 = args13;
+    }
+
+    public static String getArgs14() {
+        return args14;
+    }
+
+    public static void setArgs14(String args14) {
+        NewArgs.args14 = args14;
+    }
+
+    public static String get(int i) {
+        switch (i) {
+            case 0:
+                return args0;
+
+            case 1:
+                return args1;
+
+            case 2:
+                return args2;
+
+            case 3:
+                return args3;
+
+            case 4:
+                return args4;
+
+            case 5:
+                return args5;
+
+            case 6:
+                return args6;
+
+            case 7:
+                return args7;
+
+            case 8:
+                return args8;
+
+            case 9:
+                return args9;
+
+            case 10:
+                return args10;
+
+            case 11:
+                return args11;
+
+            case 12:
+                return args12;
+
+            case 13:
+                return args13;
+
+            case 14:
+                return args14;
+            default:
+                return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/example/script/TestJava.java
@@ -0,0 +1,19 @@
+//不能有package
+//package com.example.script;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/5/23 2:10 下午
+ */
+public class TestJava {
+
+    /**
+     * 在Terminal里进入当前目录执行: javac TestJava.java       java TestJava
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
