c4b49bced117d96db25d014eb5a7cb53570a8fc8
duanxiangqing <794055465@qq.com>
2021-05-27 11:42:32 +0800

    add

---
+++ src/main/java/com/example/atest/test6.java
@@ -0,0 +1,15 @@
+package com.example.atest;
+
+/**
+ * @author duanxiangqing
+ * @date 2021/5/26
+ */
+public class test6 {
+
+
+    public static void main(String[] args) {
+        int num=600000000;
+        System.out.println(num/8/1024/1024);
+    }
+
+}
