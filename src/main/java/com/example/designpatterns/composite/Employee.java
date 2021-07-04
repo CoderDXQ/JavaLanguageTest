9187d5aade752c2e69dd3f4279a37fa48e2676cb
cococolabear <794055465@qq.com>
2021-07-04 23:08:10 +0800

    组合模式

---
+++ src/main/java/com/example/designpatterns/composite/Employee.java
@@ -0,0 +1,36 @@
+package com.example.designpatterns.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/7/4 11:00 下午
+ */
+//组合模式使用树形结构来管理对象
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<Employee>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+}
---
+++ src/test/java/com/example/designpatterns/composite/EmployeeTest.java
@@ -0,0 +1,12 @@
+package com.example.designpatterns.composite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Duan Xiangqing
+ * @version 1.0
+ * @date 2021/7/4 11:02 下午
+ */
+class EmployeeTest {
+
+}
